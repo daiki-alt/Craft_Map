9fd1c152d5b420d9ec380b9e312e958b5db958d0
daiki-alt <sra17aa077@outlook.jp>
2021-11-04 00:05:46 +0900

    fix UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'id' => 1,
             'name' => 'sobuedaiki',
             'email' => 'sra17aa077@outlook.jp',
             'password' => bcrypt('Namikazeminato1016')
